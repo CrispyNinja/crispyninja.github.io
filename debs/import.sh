a5a2f207a3c296b1d753ad6c023537c704bb3cee
CrispyNinja <eirik1@live.no>
2017-06-05 10:43:21 +0200

    Ok now doe

--- debs/import.sh
+++ debs/import.sh
@@ -7,3 +7,10 @@ done
 dpkg-scanpackages . /dev/null > Packages && bzip2 < Packages > Packages.bz2 && gzip < Packages > Packages.gz
 
 cp Packages .. && cp Packages.bz2 .. && cp Packages.gz ..
+
+rm Packages && rm Packages.bz2 && rm Packages.gz
+
+for debs in *.deb
+do
+  rm debs
+done
