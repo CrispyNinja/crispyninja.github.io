c65ff70e88b5559799618f20aad031c02de5413e
CrispyNinja <eirik1@live.no>
2017-06-05 11:19:37 +0200

    Re

--- import.sh
+++ import.sh
@@ -12,12 +12,3 @@ do
 done
 
 dpkg-scanpackages debs/ /dev/null > Packages && bzip2 < Packages > Packages.bz2 && gzip < Packages > Packages.gz
-
-#cp debs/Packages .. && cp debs/Packages.bz2 .. && cp debs/Packages.gz ..
-
-#rm debs/Packages && rm debs/Packages.bz2 && rm debs/Packages.gz
-
-#for debs in *.deb
-#do
-#  rm debs
-#done
